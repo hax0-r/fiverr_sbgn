5e4cff4ac0bf51ce897dcc77373b3fc53e7321e9
Muhammad Talha <talha18513@gmail.com>
2025-03-16 16:55:27 +0500

    page added

---
+++ Docs/Javascript.js
@@ -0,0 +1,33 @@
+const lenis = new Lenis();
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+function openTab(event, tabId) {
+    let tabs = document.querySelectorAll('.tab');
+    let contents = document.querySelectorAll('.content');
+    
+    tabs.forEach(tab => tab.classList.remove('active'));
+    contents.forEach(content => content.classList.remove('active'));
+    
+    event.target.classList.add('active');
+    document.getElementById(tabId).classList.add('active');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.querySelector(".bottomToTop");
+    
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 200) {
+            scrollToTopBtn.style.display = "flex";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+    
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+});
